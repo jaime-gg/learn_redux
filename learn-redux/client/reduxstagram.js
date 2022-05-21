0b7a24242d075c492d12007f9f85f47de2e659c7
jester <jaimebuddy98@gmail.com>
2022-05-21 12:58:34 -0500

    set up router to render components properly

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // IMPORT CSS 
 import css from  './styles/style.styl';
 
 // IMPORT COMPONENTS 
 import Main from './components/Main'
+import PhotoGrid from './components/PhotoGrid'
+import Single from './components/Single'
 
+const router = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path='/view/:postId' component={Single}></Route> 
+        </Route>
+    </Router>
+)
 
-
-render(<Main/>, document.getElementById('root'));
+render(router, document.getElementById('root'));
