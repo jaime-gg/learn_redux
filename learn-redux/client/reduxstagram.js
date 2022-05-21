76bf7fae1148207773cb3ecfc6e33d39ced3e4af
jester <jaimebuddy98@gmail.com>
2022-05-21 12:44:03 -0500

    setup barebones for components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 
 // IMPORT CSS 
-import css from './styles/style.styl'
+import css from  './styles/style.styl';
 
 // IMPORT COMPONENTS 
 import Main from './components/Main'
