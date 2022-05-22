9e1487058d034e25ff87aebdd56a8bf9f007a144
jester <jaimebuddy98@gmail.com>
2022-05-22 12:34:45 -0500

    remove comment action

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,14 +1,13 @@
 // INCREMENT LIKES
-function increment(index) {
+export function increment(index) {
     return {
         type: 'INCREMENT_LIKES',
         index
     }
 }
 
-
 // ADD COMMENT 
-function addComment(postId, author, comment) {
+export function addComment(postId, author, comment) {
     return{
         type: 'ADD_COMMENT',
         postId,
@@ -18,3 +17,10 @@ function addComment(postId, author, comment) {
 }
 
 // REMOVE COMMENT 
+export function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT', 
+        postId,
+        i
+    }
+}
